7337e40bb91e3802e3065b3de108e81046a2c41c
Tuan Anh Ha <tuananhha@Tuans-MBP.fritz.box>
2022-08-09 21:21:01 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import Home from './pages/home'
+import Create from './pages/create'
+import Translate from './pages/translate'
+import Summary from './pages/summary'
+import ChipExample from './pages/chip/chip'
+
+import { CustomerProvider } from './context/CustomerContext'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <CustomerProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/create' element={<Create />} />
+          <Route path='/translate' element={<Translate />} />
+          <Route path='/summary' element={<Summary />} />
+          <Route path='/chip-example' element={<ChipExample />} />
+        </Routes>
+      </BrowserRouter>
+    </CustomerProvider>
+  )
 }
 
-export default App;
+export default App
